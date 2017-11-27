6f240442843ba1cf6a8189ddef29d80f50e9bc9f
WestwardWinds <34014603+WestwardWinds@users.noreply.github.com>
2017-11-26 21:53:13 -0600

    v0.1

--- js/main.js
+++ js/main.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
     });
 
     $("#clients-logo").owlCarousel({
- 
+
         itemsCustom : false,
         pagination : false,
         items : 5,
@@ -93,10 +93,12 @@ $(".fancybox").fancybox({
     closeEffect : 'elastic',
     closeSpeed  : 350,
 
+    type: 'iframe',
+
     closeClick : true,
     helpers : {
-        title : { 
-            type: 'inside' 
+        title : {
+            type: 'inside'
         },
         overlay : {
             css : {
@@ -105,14 +107,3 @@ $(".fancybox").fancybox({
         }
     }
 });
-
-
-
-
-
-
- 
-
-
-
-
